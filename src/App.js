c21319c7cf5b47f2ef86d4f16fa7b2677b44ab98
FyreFox360 <jmge5833@gmail.com>
2023-03-17 14:29:51 -0600

    Agregando gestion de rutas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 export default function App() {
   return (
+    <>
     <h1>this is a test</h1>
+
+      <div>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+        
+      </div>
+     </>
+  
   )
 }
